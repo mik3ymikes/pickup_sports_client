5be971d52ac703b2719c0d368e3d9598e07d0960
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-02-14 21:22:16 -0600

    32 min

--- src/app/features/events/events.component.ts
+++ src/app/features/events/events.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EventService } from '../../core/services/event.service';
+import { Event } from '../../shared/models/event';
 
 @Component({
   selector: 'app-events',
@@ -7,6 +9,39 @@ import { Component } from '@angular/core';
   templateUrl: './events.component.html',
   styleUrl: './events.component.css'
 })
-export class EventsComponent {
+export class EventsComponent implements OnInit {
+currentPage:number=1
+totalPages:number=0;
+events:Event[]=[]
 
+constructor(private eventService:EventService) {}
+
+  ngOnInit(): void {
+      this.loadEvents(this.currentPage)
+  }
+  loadEvents(page:number){
+    this.eventService.getEvents(page).subscribe({
+      next: (response:any) =>{
+        this.events=response.events;
+        this.currentPage=response.current_page;
+        this.totalPages=response.total_pages;
+        console.log(this.events, this.currentPage, this.totalPages)
+      },
+      error: (error:any) =>{
+        console.error("errror fetching events", error)
+      }
+    })
+  }
+
+  nextPage(){
+    if(this.currentPage<this.totalPages){
+      this.loadEvents(this.currentPage +1)
+    }
+  }
+
+  previousPage(){
+    if(this.currentPage>1){
+      this.loadEvents(this.currentPage-1)
+    }
+  }
 }
