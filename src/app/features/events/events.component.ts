e445c9626af6c1a900a45fe47fabd56392db23ce
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-02-15 14:16:44 -0600

    completed pagination and all vids

--- src/app/features/events/events.component.ts
+++ src/app/features/events/events.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { EventService } from '../../core/services/event.service';
 import { Event } from '../../shared/models/event';
 import { EventComponent } from '../../shared/components/events/event/event.component';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-events',
@@ -15,10 +16,14 @@ currentPage:number=1
 totalPages:number=0;
 events:Event[]=[]
 
-constructor(private eventService:EventService) {}
+constructor(private eventService:EventService,
+   private router:Router, private route:ActivatedRoute) {}
 
   ngOnInit(): void {
-      this.loadEvents(this.currentPage)
+      this.route.queryParams.subscribe(params=>{
+        const page=params['page'] ? Number(params['page']) :1
+        this.loadEvents(page)
+      })
   }
   loadEvents(page:number){
     this.eventService.getEvents(page).subscribe({
@@ -36,13 +41,21 @@ constructor(private eventService:EventService) {}
 
   nextPage(){
     if(this.currentPage<this.totalPages){
-      this.loadEvents(this.currentPage +1)
+      this.router.navigate([], {
+        relativeTo:this.route,
+        queryParams: {page:this.currentPage +1},
+        queryParamsHandling: 'merge'
+      })
     }
   }
 
   previousPage(){
     if(this.currentPage>1){
-      this.loadEvents(this.currentPage-1)
+      this.router.navigate([], {
+        relativeTo:this.route,
+        queryParams: {page:this.currentPage -1},
+        queryParamsHandling: 'merge'
+      })
     }
   }
 }
