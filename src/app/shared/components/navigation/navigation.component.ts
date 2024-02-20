884003d5e418c4570f708eb23301e75b426ae738
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-02-19 19:59:18 -0600

    completed third video adding logut to navbar

--- src/app/shared/components/navigation/navigation.component.ts
+++ src/app/shared/components/navigation/navigation.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AuthenticationService } from '../../../core/services/authentication.service';
 
 @Component({
   selector: 'app-navigation',
@@ -10,6 +11,19 @@ import { RouterModule } from '@angular/router';
 })
 export class NavigationComponent {
   isSidebarVisible:boolean=false;
+
+
+  constructor(private authService:AuthenticationService){}
+
+
+  isLoggedIn(){
+    return this.authService.isLoggedIn()
+  }
+
+  logout(){
+    this.authService.logout()
+    this.toggleSidebar()
+  }
 
   toggleSidebar(){
     this.isSidebarVisible=!this.isSidebarVisible
